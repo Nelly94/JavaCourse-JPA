53d413c5ba4718c36034b545dcd6affa8a918b1e
nellyiriberri <nellyiriberri@gmail.com>
2020-05-29 17:02:07 +0200

    Create table person

--- src/main/java/model/Artist.java
+++ src/main/java/model/Artist.java
@@ -5,17 +5,7 @@ import java.util.List;
 
 @Entity
 @Table(name="ARTIST")
-public class Artist {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(name="first_name")
-    private String firstName;
-
-    @Column(name="last_name")
-    private String lastName;
+public class Artist extends Person{
 
     @Column(name="band_name")
     private String bandName;
@@ -45,30 +35,6 @@ public class Artist {
         this.firstName = firstName;
         this.lastName = lastName;
         this.bandName = bandName;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
     }
 
     public String getBandName() {
--- src/main/java/model/Manager.java
+++ src/main/java/model/Manager.java
@@ -5,44 +5,10 @@ import java.util.List;
 
 @Entity
 @Table(name="MANAGER")
-public class Manager {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(name="first_name")
-    private String firstName;
-
-    @Column(name="last_name")
-    private String lastName;
+public class Manager extends Person{
 
     @OneToMany(mappedBy = "manager")
     private List<Artist> representedArtists;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
 
     public List<Artist> getRepresentedArtists() {
         return representedArtists;
