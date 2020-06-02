76ec6627d7e38da7a0a9d9280b886d40b1cb5853
nellyiriberri <nellyiriberri@gmail.com>
2020-06-02 12:21:08 +0200

    Delete manager

--- src/main/java/Entry.java
+++ src/main/java/Entry.java
@@ -17,6 +17,10 @@ public class Entry {
         manager.setLastName("Ramirez");
         artist.setManager(manager);
 
+        Manager manager2 = new Manager();
+        manager2.setFirstName("Luis");
+        manager2.setLastName("Santos");
+
         Instrument piano = new Instrument();
         piano.setName("Piano");
         piano.setType(InstrumentType.STRING);
@@ -32,12 +36,17 @@ public class Entry {
             em.persist(manager);
             em.persist(piano);
             em.persist(artist);
+            em.persist(manager2);
             em.persist(media);
-            transaction.commit();
+
 
             Media mediaFounded = em.find(Media.class, new MediaId("insoportable", MediaType.CD));
             System.out.println("Media founded: " + mediaFounded.getName()+ " " + mediaFounded.getArtist().getBandName());
 
+            Manager managerToDelete = em.find(Manager.class, manager2.id);
+            em.remove(managerToDelete);
+
+            transaction.commit();
 
         }catch(Exception e){
             transaction.rollback();
